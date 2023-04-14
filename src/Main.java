f2b977b4391745046b466bb031d01ce5877a2d7c
Veronica <almeida.vrc@gmail.com>
2023-04-14 11:55:04 -0300

    Adds purchase calculation and displays at the end

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String limitMenu = "Digite o limite do cartão: ";
+        String ProductMenu = "Digite a descrição da compra: ";
+        String ValueMenu = "Digite o valor da compra: ";
+        String menuOut = "\n Digite 0 para sair ou 1 para continuar";
+        String product;
+        int option = 1;
+        double price;
+        double limit;
+        Scanner input = new Scanner(System.in);
+        ArrayList<Shopping> total = new ArrayList<>();
+
+        System.out.println(limitMenu);
+        limit = input.nextInt();
+
+
+        while (option != 0) {
+            Shopping shopping  = new Shopping(limit);
+
+            System.out.println(ProductMenu);
+            product = input.next();
+
+            System.out.println(ValueMenu);
+            price = input.nextInt();
+
+            if (limit >= price) {
+
+                shopping.setProduct(product);
+                shopping.setPrice(price);
+
+                shopping.addShopping(price);
+                shopping.setBalance(price);
+                limit -= price;
+
+                total.add(shopping);
+
+            } else {
+
+                System.out.println("Saldo Insuficiente");
+                break;
+            }
+
+            System.out.println(menuOut);
+            option = input.nextInt();
+
+        }
+
+        total.sort(Comparator.comparing(Shopping::getPrice));
+
+        System.out.println("**********************************");
+        System.out.println("COMPRAS REALIZADAS: \n");
+
+        for (Shopping item:total) {
+            System.out.println(item.getProduct() + " - " + item.getPrice());
+        }
+
+        System.out.println("\n**********************************");
+        System.out.println("\n Saldo do cartão: " + limit);
+
     }
 }
---
+++ src/Shopping.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Shopping {
+    private String product;
+    private double price;
+    private double limit;
+    private double balance;
+
+    public Shopping(double limit) {
+        this.limit = limit;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public void addShopping(double price){
+        this.limit -= price;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
